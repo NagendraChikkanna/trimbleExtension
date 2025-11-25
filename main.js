f9384a6081d55e04aad6588d50d16f9c53358d0f
Nagendra C <nagendra@virtuele.us>
2025-11-25 17:32:06 +0530

    Update

--- main.js
+++ main.js
@@ -1,9 +1,51 @@
+async function initializeTrimbleConnect(){
+    let workSpaceAPI = await TrimbleConnectWorkspace.connect(window.parent,
+        (event, args) => {
+            switch (event) {
+                case "extension.command":
+                //"Command executed by the user: args.data"
+                    break;
+                case "extension.accessToken":
+                //"Accestoken or status: args.data"
+                    break;
+                case "extension.userSettingsChanged":
+                //"User settings changed!"
+                    break;
+                default:
+            }
+        },
+        3000 // connection timeout in milliseconds.
+        );
+    console.log(workSpaceAPI);
+
+    const mainMenuObject = {
+        title: "Test extension app",
+        icon: "http://example.com/main_menu_icon.png",
+        command: "main_nav_menu_cliked",
+        subMenus: [
+            {
+            title: "Sub menu 1",
+            icon: "http://example.com/sub_menu_icon.png",
+            command: "submenu_1_clicked",
+            },
+            {
+            title: "Sub menu 2",
+            icon: "http://example.com/sub_menu_icon.png",
+            command: "submenu_2_clicked",
+            },
+        ],
+    };
+
+    // Updating the menu object.
+    workSpaceAPI.ui.setMenu(mainMenuObject);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('redirectButton');
 
     button.addEventListener('click', () => {
         // Open external link in a new tab
-        window.open('https://dev.virtuele.us', '_blank');
+        window.open('https://dev.virtuele.us', '_blank');        
     });
 
     // Optional: If you want to integrate with Trimble Connect UI API
@@ -18,4 +60,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-
+initializeTrimbleConnect();
